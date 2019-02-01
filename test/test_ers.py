6387b89728900a4805a82bd794d60db370d5a095
Jonathan Karras <karrots@users.noreply.github.com>
2019-02-01 16:40:43 -0700

    Add basic framework for unittest

--- test/test_ers.py
+++ test/test_ers.py
@@ -1,8 +1,11 @@
+# python -m unittest discover -s ../test
 from cream import ERS
 import json
 
+import unittest
 from unittest import TestCase
 from unittest.mock import patch, Mock
+
 
 class ErsTest(TestCase):
 
@@ -13,6 +16,17 @@ class ErsTest(TestCase):
         result = self.ise._mac_test('24:be:05:0b:01:ab')
 
         self.assertTrue(result)
+
+    def test_pass_ersresponse_error(self):
+        result = self.ise._pass_ersresponse()
+
+        self.asertTrue(result['error'])
+
+    def test_pass_ersresponse_response(self):
+        result = self.ise._pass_ersresponse()
+
+        self.asertTrue(result['response'])
+
 
 if __name__ == '__main__':
     unittest.main()
