698950270e24e709bc133da6db026950c3d80f44
Marc Sommerhalder <marc.sommerhalder@ins.hsr.ch>
2019-11-19 13:08:00 +0100

    Adds missing setup configuration

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='ISE',
     version='1.0.0',
+    py_modules=['ise'],
     url='',
     license='LICENSE.md',
     maintainer='Jonathan Karras',
