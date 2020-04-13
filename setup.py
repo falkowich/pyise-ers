aac93688a7c212e9c20fb57dd7e5a3503d1d828e
Falk <gammalget@gmail.com>
2020-04-13 19:35:32 +0200

    did some fixes from https://packaging.python.org/guides/making-a-pypi-friendly-readme/

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='ISE',
@@ -14,7 +16,8 @@ setup(
     maintainer='Andreas Falk',
     maintainer_email='falk@sadsloth.net',
     description='Python module to manage Cisco ISE via the ERS API',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
