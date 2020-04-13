63e6b901f90f23776fd6860aeb4a046ae131fd22
Falk <gammalget@gmail.com>
2020-04-13 18:52:23 +0200

    setup.py fixes

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ setup(
     name='ISE',
     version='1.1.0',
     py_modules=['ise'],
-    url='',
+    url='https://github.com/falkowich/ise',
     license='LICENSE.md',
     maintainer='Andreas Falk',
     maintainer_email='falk@sadsloth.net',
-    description='',
+    description='Python module to manage Cisco ISE via the ERS API',
     long_description='README.md',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'furl>=1.2.1',
-        'requests>=2.18.4'
+        'furl>=2.1.0',
+        'requests>=2.23.0'
     ],
     extras_require={
         'test': [
