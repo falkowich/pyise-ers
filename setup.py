0536098679553e0b23740279be4357b1fc516ffc
Falk <andreas.falk@nykoping.se>
2020-04-14 10:52:26 +0200

    ...

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='cisco-ise',
+    name='ISE',
     version='0.1.0',
     py_modules=['ise'],
     url='https://github.com/falkowich/ise',
