e85ee2139fd4de604f7298625235a79699133c3f
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2020-05-05 09:37:09 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ISE',
-    version='0.1.0.1',
+    version='0.1.1',
     py_modules=['ise'],
     url='https://github.com/falkowich/ise',
     download_url='https://pypi.python.org/pypi/ise',
