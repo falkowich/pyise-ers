9628fedae69165618d4d8f3cd15260c635e03245
Falk <gammalget@gmail.com>
2020-05-30 23:28:50 +0200

    unittesting with pytest phase one done

--- test/test_ise.py
+++ test/test_ise.py
@@ -122,3 +122,199 @@ def test_delete_user():  # noqa D103
     r1 = ise.delete_user(user['user_id'])
     assert r1['success'] is True
     # TODO assert r1['response'] == [('ADM', 'b7ef3920-ca30-11e9-92d6-96c5f2507fd6', 'Admin network - [MAB]')]
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_device_groups.yaml')
+def test_get_device_groups():
+    r1 = ise.get_device_groups(size=1, page=1)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_device_group.yaml')
+def test_get_device_group():
+    r1 = ise.get_device_group(device_group['oid'])
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/add_device.yaml')
+def test_add_device():
+    r1 = ise.add_device(
+        name=device['name'],
+        ip_address=device['ip_address'],
+        radius_key=device['radius_key'],
+        snmp_ro=device['snmp_ro'],
+        dev_group=device['dev_group'],
+        dev_location=device['dev_location'],
+        dev_type=device['dev_type'],
+        description=device['description'],
+        snmp_v=device['snmp_v'],
+        dev_profile=device['dev_profile']
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_devices.yaml')
+def test_get_devices():
+    r1 = ise.get_devices(size=1, page=1)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_device.yaml')
+def test_get_device():
+    r1 = ise.get_device(device['name'])
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/delete_device.yaml')
+def test_delete_device():
+    r1 = ise.delete_device(device['name'])
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_sgts.yaml')
+def test_get_sgts():
+    r1 = ise.get_sgts(size=1, page=1)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_sgt.yaml')
+def test_get_sgt():
+    r1 = ise.get_sgt("Unknown")
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/add_sgt.yaml')
+def test_add_sgt():
+    r1 = ise.add_sgt(
+        name="Python_Unit_Test",
+        description="Unit Tests",
+        value=trustsec["test_sgt_value"],
+        return_object=True
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/update_sgt.yaml')
+def test_update_sgt():
+    res = ise.get_sgt("Python_Unit_Test")
+    id = res['response']['id']
+    r1 = ise.update_sgt(
+        id,
+        name="Test_Unit_Python",
+        description="Python Unit Tests",
+        value=trustsec["test_sgt_value"]
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/delete_sgt.yaml')
+def test_delete_sgt():
+    res = ise.get_sgt("Test_Unit_Python")
+    id = res['response']['id']
+    r1 = ise.delete_sgt(id)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_sgacls.yaml')
+def test_get_sgacls():
+    r1 = ise.get_sgacls(size=1, page=1)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_sgacl.yaml')
+def test_get_sgacl():
+    r1 = ise.get_sgacl("Permit IP")
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/add_sgacl.yaml')
+def test_add_sgacl():
+    r1 = ise.add_sgacl(
+        name="Python_Unit_Test",
+        description="Unit Tests",
+        ip_version="IPV4",
+        acl_content=["permit ip"],
+        return_object=True
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/add_sgacl.yaml')
+def test_add_sgacl():
+    r1 = ise.add_sgacl(
+        name="Python_Unit_Test",
+        description="Unit Tests",
+        ip_version="IPV4",
+        acl_content=["permit ip"],
+        return_object=True
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/update_sgacl.yaml')
+def test_update_sgacl():
+    res = ise.get_sgacl("Python_Unit_Test")
+    id = res['response']['id']
+    r1 = ise.update_sgacl(
+        id,
+        name="Test_Unit_Python",
+        description="Python Unit Tests",
+        ip_version="IPV4",
+        acl_content=["permit ip"]
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/delete_sgacl.yaml')
+def test_delete_sgacl():
+    res = ise.get_sgacl("Test_Unit_Python")
+    id = res['response']['id']
+    r1 = ise.delete_sgacl(id)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_egressmatrixcells.yaml')
+def test_get_egressmatrixcells():
+    r1 = ise.get_egressmatrixcells(size=1, page=1)
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/get_egressmatrixcell.yaml')
+def test_get_egressmatrixcell():
+    r1 = ise.get_egressmatrixcell("Default egress rule")
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/add_egressmatrixcell.yaml')
+def test_add_egressmatrixcell():
+    r1 = ise.add_egressmatrixcell(
+        trustsec["emc_source_sgt"],
+        trustsec["emc_dest_sgt"],
+        "PERMIT_IP",
+        description="Python Unit Tests",
+        return_object=True
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/update_egressmatrixcell.yaml')
+def test_update_egressmatrixcell():
+    res = ise.get_egressmatrixcell("Python Unit Tests")
+    id = res['response']['id']
+    r1 = ise.update_egressmatrixcell(
+        id,
+        trustsec["emc_source_sgt"],
+        trustsec["emc_dest_sgt"],
+        "NONE",
+        description="Test_Unit_Python",
+        acls=[trustsec["test_assign_acl"]]
+    )
+    assert r1['success'] is True
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/delete_egressmatrixcell(id).yaml')
+def test_delete_egressmatrixcell():
+    res = ise.get_egressmatrixcell("Test_Unit_Python")
+    id = res['response']['id']
+    r1 = ise.delete_egressmatrixcell(id)
+    assert r1['success'] is True
